3d1eec28325fd98abe9e02834311b1dd41cf3b30
Sakshat682 <56864491+Sakshat682@users.noreply.github.com>
2021-03-11 14:00:21 +0530

    Condition for cng added in app.py

--- app.py
+++ app.py
@@ -26,9 +26,12 @@ def predict():
         if(Fuel_Type_Petrol=='Petrol'):
                 Fuel_Type_Petrol=1
                 Fuel_Type_Diesel=0
-        else:
+        elif(Fuel_Type_Petrol=='Diesel'):
             Fuel_Type_Petrol=0
             Fuel_Type_Diesel=1
+        else:
+            Fuel_Type_Petrol=0
+            Fuel_Type_Diesel=0
         Year=2021-Year
         Seller_Type_Individual=request.form['Seller_Type_Individual']
         if(Seller_Type_Individual=='Individual'):
